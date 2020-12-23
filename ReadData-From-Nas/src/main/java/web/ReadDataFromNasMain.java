726b206c2927b66e3ea77cb02a1e44c90f8581e8
dafeng.guo <dafeng..666>
2020-12-23 17:40:15 +0800

    main方法获取java -jar 启动时所带参数值，同时测试key value形式获取本地磁盘文件内容

--- ReadData-From-Nas/src/main/java/web/ReadDataFromNasMain.java
+++ ReadData-From-Nas/src/main/java/web/ReadDataFromNasMain.java
@@ -1,8 +1,18 @@
 package web;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
 
 /**
  * @author : dafeng.guo
@@ -15,7 +25,62 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 
 public class ReadDataFromNasMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(ReadDataFromNasMain.class,args);
+
+        //Java -Durl="D:/test/test.txt" -jar name.jar
+        /*String contents=System.getProperty("url");  //以jar启动获取参数传入的路径
+        ToExcel(contents);*/
+    }
+
+    private static void ToExcel(String pathUrl) throws IOException {
+
+        //创建excel工作簿
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        //定义表头
+        String[] title = {"key","value"};
+        //创建工作表
+        XSSFSheet sheet = workbook.createSheet();
+        //创建第一行表头
+        XSSFRow row = sheet.createRow(0);
+        XSSFCell cell = null;
+        //插入第一行数据的表头
+        for(int i = 0; i < title.length; i++){
+            cell = row.createCell(i);
+            cell.setCellValue(title[i]);
+        }
+
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("0","881,991");
+        hashMap.put("1","882,992");
+        hashMap.put("2","883,993");
+        hashMap.put("3","883,994");
+        hashMap.put("4","xxxxxxxxxx,yyyyyyyyyyyyyyyyy");
+        for (int i=0;i<hashMap.size();i++){
+            String s = hashMap.get(String.valueOf(i));
+            String[] split = s.split(",");
+            XSSFRow row1 = sheet.createRow(i+1);
+            XSSFCell cell1 = row1.createCell(0);
+            cell1.setCellValue(split[0]);
+            XSSFCell cell2 = row1.createCell(1);
+            cell2.setCellValue(split[1]);
+        }
+
+        //创建excel文件
+        File file=new File(pathUrl+".xlsx");
+        FileOutputStream stream=null;
+        try {
+            file.createNewFile();
+            //将excel写入
+            stream= FileUtils.openOutputStream(file);
+            workbook.write(stream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if (stream != null){
+                stream.close();
+            }
+        }
+
     }
 }
--- ReadData-From-Nas/src/main/java/web/controller/ReadController.java
+++ ReadData-From-Nas/src/main/java/web/controller/ReadController.java
@@ -1,13 +1,24 @@
 package web.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import web.service.TestService;
 
 import javax.annotation.Resource;
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.*;
+
+import org.apache.commons.io.FileUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 /**
  * @author : dafeng.guo
@@ -50,12 +61,15 @@ public class ReadController {
         return i;
     }
 
-
-    @GetMapping("/readData")
+    /**
+     * 读取文件内容
+     * @throws Exception
+     */
+    /*@GetMapping("/readData")
     public void readData() throws Exception {
         FileInputStream fileInputStream=null;
         try {
-            String url="D://test//test.txt";
+            String url="D:/test/test.txt";
             File file = new File(url);
             if (!file.exists()){
                 throw new Exception("您访问的文件不存在");
@@ -74,5 +88,130 @@ public class ReadController {
                 fileInputStream.close();
             }
         }
+    }*/
+
+    /**
+     * 遍历文件夹
+     */
+    @GetMapping("/selectContents")
+    public void selectContents() throws Exception {
+        java.lang.String contents="D:/test";
+        File file = new File(contents);
+        getContents(file);
+    }
+
+    /**
+     * 遍历文件夹目录
+     * @param file
+     * @throws Exception
+     */
+    public void getContents(File file) throws Exception{
+        if (!file.exists()){
+            throw new Exception("Sorry,您访问的文件不存在！！！");
+        }
+        File[] files = file.listFiles();
+        for (File f : files){
+            if (f.isDirectory()){
+                getContents(f);
+            }else{
+                readFileInfo(f);
+            }
+        }
+    }
+
+    public void readFileInfo(File file) throws Exception{
+        String name = file.getName();
+
+        log.info(name+"文件内容如下");
+        FileInputStream fis=null;
+        try {
+            fis=new FileInputStream(file);
+            byte[] bytes = new byte[1024];
+            int len ;
+            while ((len = fis.read(bytes)) != -1){
+                String info = new String(bytes, 0, len);
+                log.info(info);
+            }
+            log.info(name+"文件内容读取结束======================================");
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if (fis != null){
+                fis.close();
+            }
+        }
+    }
+
+    @GetMapping("/testWriteMapToExcel")
+    public void testWriteMapToExcel() throws IOException {
+        String contents="D:/test";
+        ToExcel(contents);
+       /* Date currentDate = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        log.info(sdf.format(currentDate));*/
+
+    }
+
+    private static void ToExcel(String pathUrl) throws IOException {
+
+        //创建excel工作簿
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        //定义表头
+        String[] title = {"key","value"};
+        //创建工作表
+        XSSFSheet sheet = workbook.createSheet();
+        //创建第一行表头
+        XSSFRow row = sheet.createRow(0);
+        XSSFCell cell = null;
+        //插入第一行数据的表头
+        for(int i = 0; i < title.length; i++){
+            cell = row.createCell(i);
+            cell.setCellValue(title[i]);
+        }
+
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("0","881,991");
+        hashMap.put("1","882,992");
+        hashMap.put("2","883,993");
+        hashMap.put("3","883,994");
+        for (int i=0;i<hashMap.size();i++){
+            String s = hashMap.get(String.valueOf(i));
+            String[] split = s.split(",");
+            XSSFRow row1 = sheet.createRow(i+1);
+            XSSFCell cell1 = row1.createCell(0);
+            cell1.setCellValue(split[0]);
+            XSSFCell cell2 = row1.createCell(1);
+            cell2.setCellValue(split[1]);
+        }
+        
+        //创建excel文件
+        File file=new File(pathUrl+".xlsx");
+        FileOutputStream stream=null;
+        try {
+            file.createNewFile();
+            //将excel写入
+            stream= FileUtils.openOutputStream(file);
+            workbook.write(stream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if (stream != null){
+                stream.close();
+            }
+        }
+
+    }
+
+    /**
+     * 以key value形式获取文件内容
+     */
+    @GetMapping("/testProperties")
+    public  void testProperties() throws Exception{
+
+        Properties properties = new Properties();
+        InputStream resourceAsStream = new FileInputStream("D:/test.txt");
+        properties.load(resourceAsStream);
+        Object key1 = properties.get("key1");
+        System.out.println("9999999999");
     }
 }
