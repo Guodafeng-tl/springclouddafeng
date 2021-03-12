09df3f521948832d4b17157e8fc8e4c2b30923d4
dafeng.guo <dafeng..666>
2021-03-12 15:04:01 +0800

    springboot整合kafka

--- SpringBoot-Kafka9000/src/main/java/kafka/controller/Consumer.java
+++ SpringBoot-Kafka9000/src/main/java/kafka/controller/Consumer.java
@@ -2,6 +2,7 @@ package kafka.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -9,11 +10,12 @@ import org.springframework.stereotype.Component;
  * @author : dafeng.guo
  * @date : 13:57 2020/12/29
  **/
+@Configuration
 @Component
 @Slf4j
 public class Consumer {
-
-    @KafkaListener(topics = "demo")
+    private final String TOPIC="dafeng-kafka";
+    @KafkaListener(topics = TOPIC)
     public void listen(ConsumerRecord< ? , ? > record){
         log.info(record.topic()+" "+record.value());
     }
--- SpringBoot-Kafka9000/src/main/java/kafka/controller/ProductController.java
+++ SpringBoot-Kafka9000/src/main/java/kafka/controller/ProductController.java
@@ -1,8 +1,10 @@
 package kafka.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -12,15 +14,45 @@ import javax.annotation.Resource;
  * @date : 13:55 2020/12/29
  **/
 @RestController
+@Slf4j
 public class ProductController {
 
     @Resource
     KafkaTemplate<String,String> kafkaTemplate;
 
-    @GetMapping("/sendMsg")
-    public String send(String message){
-        kafkaTemplate.send("demo",message);
-        return "SUCESS";
+    private final String TOPIC="dafeng-kafka";
+
+    /**
+     * 同步发送
+     * @param message
+     * @return
+     */
+    @GetMapping("/sendMsg/{message}")
+    @Transactional(rollbackFor = Exception.class)
+    public String send(@PathVariable String message){
+        kafkaTemplate.send(TOPIC,message);
+        log.info("同步发送成功topic:" + TOPIC + " message:" +message);
+        return "同步发送成功"+" "+message;
+    }
+
+    /**
+     * 异步发送
+     * @param message
+     * @return
+     */
+    @GetMapping("/asySendMsg/{message}")
+    @Transactional(rollbackFor = Exception.class)
+    public String asySend(@PathVariable String message){
+        kafkaTemplate.send(TOPIC, message).addCallback(success -> {
+            String topic = success.getRecordMetadata().topic();
+            int partition = success.getRecordMetadata().partition();
+            long offset = success.getRecordMetadata().offset();
+            log.info("异步发送成功："+"topic:" + topic + " partition:" + partition + " offset:" + offset);
+        },failure -> {
+            String failureInfo = failure.getMessage();
+            log.info("异步发送失败："+failureInfo);
+        });
+        return "异步发送成功"+" "+message;
     }
 
 }
