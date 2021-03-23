08ce97981b21de210ca8484a4766df96e34fcca3
dafeng.guo <dafeng..666>
2021-03-23 14:08:26 +0800

    本地测试修改

---
+++ ReadData-From-Nas/src/main/java/web/dao/Teacher.java
@@ -0,0 +1,18 @@
+package web.dao;
+
+/**
+ * @author : dafeng.guo
+ * @date : 16:19 2021/2/24
+ **/
+public class Teacher {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
